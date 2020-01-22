f8855cfa387617509b67348a7dfbda8cea56ac7f
Abdulkadir  Ahmed <abdulk40@ug180.eecg.toronto.edu>
2020-01-21 22:22:00 -0500

    Set up command line interface for KV Client

--- src/app_kvClient/KVClient.java
+++ src/app_kvClient/KVClient.java
@@ -1,16 +1,250 @@
 package app_kvClient;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import logger.LogSetup;
+
 import client.KVCommInterface;
+import client.KVStore;
+import shared.messages.KVMessage;;
 
 public class KVClient implements IKVClient {
-    @Override
-    public void newConnection(String hostname, int port) throws Exception{
-        // TODO Auto-generated method stub
+
+    private static Logger logger = Logger.getRootLogger();
+    private static final String PROMPT = "M1_Client> ";
+    private BufferedReader stdin;
+    private boolean stop = false;
+
+    private String serverAddress;
+    private int serverPort;
+
+    private KVStore store = null;
+
+    public void run() {
+        while (!stop) {
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print(PROMPT);
+
+            try {
+                String cmdLine = stdin.readLine();
+                this.handleCommand(cmdLine);
+            } catch (IOException e) {
+                stop = true;
+
+                printError("CLI does not respond - Application terminated ");
+            }
+
+        }
+    }
+
+    private void handleCommand(String cmdLine) {
+        String[] tokens = cmdLine.split("\\s+");
+
+        if (tokens[0].equals("quit")) {
+            stop = true;
+            store.disconnect();
+            System.out.println(PROMPT + "Application exit!");
+
+        } else if (tokens[0].equals("connect")) {
+            if (tokens.length == 3) {
+                try {
+                    serverAddress = tokens[1];
+                    serverPort = Integer.parseInt(tokens[2]);
+                    newConnection(serverAddress, serverPort);
+                } catch (NumberFormatException nfe) {
+                    printError("No valid address. Port must be a number!");
+                    logger.info("Unable to parse argument <port>", nfe);
+                } catch (UnknownHostException e) {
+                    printError("Unknown Host!");
+                    logger.info("Unknown Host!", e);
+                } catch (IOException e) {
+                    printError("Could not establish connection!");
+                    logger.warn("Could not establish connection!", e);
+                } catch (Exception e) {
+                    printError("Unknown exception!");
+                    logger.warn("Unknown exception!", e);
+                }
+            } else {
+                printError("Invalid number of parameters!");
+            }
+
+        } else if (tokens[0].equals("put")) {
+            if (tokens.length == 3) {
+                try {
+                    String key = tokens[1];
+                    String value = tokens[2];
+                    KVMessage reply = store.put(key, value);
+                    if (reply.getStatus() == KVMessage.StatusType.PUT_SUCCESS) {
+                        printReply("PUT request was successful! Key value tuple has been created");
+                    } else if (reply.getStatus() == KVMessage.StatusType.PUT_UPDATE) {
+                        printReply("PUT request was successful! Key value tuple has been updated");
+                    } else if (reply.getStatus() == KVMessage.StatusType.PUT_ERROR) {
+                        printError("PUT request encountered an error");
+                    } else {
+                        printError("PUT request received unknown reply");
+                    }
+                } catch (UnknownHostException e) {
+                    printError("Unknown Host!");
+                    logger.info("Unknown Host!", e);
+                } catch (IOException e) {
+                    printError("Could not establish connection!");
+					logger.warn("Could not establish connection!", e);
+                } catch (Exception e) {
+                    printError("Unknown exception!");
+                }
+            } else {
+                printError("Invalid number of parameters!");
+            }
+
+        } else if (tokens[0].equals("get")) {
+            if (tokens.length == 2) {
+                try {
+                    String key = tokens[1];
+                    KVMessage reply = store.get(key);
+                    if (reply.getStatus() == KVMessage.StatusType.GET_SUCCESS) {
+                        printReply("GET successful, retrieved \"" + reply.getValue() + "\"");
+                    } else if (reply.getStatus() == KVMessage.StatusType.GET_ERROR) {
+                        printError("GET request encountered an error");
+                    } else {
+                        printError("GET request received unknown reply");
+                    }
+                } catch (UnknownHostException e) {
+                    printError("Unknown Host!");
+                    logger.info("Unknown Host!", e);
+                } catch (IOException e) {
+                    printError("Could not establish connection!");
+					logger.warn("Could not establish connection!", e);
+                } catch (Exception e) {
+                    printError("Unknown exception!");
+                }
+            } else {
+                printError("Invalid number of parameters!");
+            }
+
+        } else if(tokens[0].equals("disconnect")) {
+            store.disconnect();
+
+        } else if(tokens[0].equals("logLevel")) {
+            if(tokens.length == 2) {
+                String level = setLevel(tokens[1]);
+                if(level.equals(LogSetup.UNKNOWN_LEVEL)) {
+                    printError("No valid log level!");
+                    printPossibleLogLevels();
+                } else {
+                    System.out.println(PROMPT +
+                            "Log level changed to level " + level);
+                }
+            } else {
+                printError("Invalid number of parameters!");
+            }
+
+        } else if(tokens[0].equals("help")) {
+            printHelp();
+        } else {
+            printError("Unknown command. Use command \"help\" to learn more.");
+        }
     }
 
     @Override
-    public KVCommInterface getStore(){
-        // TODO Auto-generated method stub
-        return null;
+    public void newConnection(String hostname, int port) throws Exception {
+
+        store = new KVStore(hostname, port);
+        store.connect();
+    }
+
+    @Override
+    public KVCommInterface getStore() {
+
+        return store;
+    }
+    
+    private void printHelp() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PROMPT).append("KV CLIENT HELP (Usage):\n");
+        sb.append(PROMPT);
+        sb.append("::::::::::::::::::::::::::::::::");
+        sb.append("::::::::::::::::::::::::::::::::\n");
+        sb.append(PROMPT).append("connect <host> <port>");
+        sb.append("\t establishes a connection to a server\n");
+        sb.append(PROMPT).append("get <key> ");
+        sb.append("\t\t\t gets value associated with key from the server\n");
+        sb.append(PROMPT).append("put <key> <value>");
+        sb.append("\t\t creates or updates key value pair in the server\n");
+        sb.append(PROMPT).append("disconnect");
+        sb.append("\t\t\t disconnects from the server \n");
+
+        sb.append(PROMPT).append("logLevel");
+        sb.append("\t\t\t changes the logLevel \n");
+        sb.append(PROMPT).append("\t\t\t\t ");
+        sb.append("ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF \n");
+
+        sb.append(PROMPT).append("quit");
+        sb.append("\t\t\t\t exits the program");
+        System.out.println(sb.toString());
+    }
+
+    private void printPossibleLogLevels() {
+        System.out.println(PROMPT
+                + "Possible log levels are:");
+        System.out.println(PROMPT
+                + "ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF");
+    }
+
+    private String setLevel(String levelString) {
+
+        if(levelString.equals(Level.ALL.toString())) {
+            logger.setLevel(Level.ALL);
+            return Level.ALL.toString();
+        } else if(levelString.equals(Level.DEBUG.toString())) {
+            logger.setLevel(Level.DEBUG);
+            return Level.DEBUG.toString();
+        } else if(levelString.equals(Level.INFO.toString())) {
+            logger.setLevel(Level.INFO);
+            return Level.INFO.toString();
+        } else if(levelString.equals(Level.WARN.toString())) {
+            logger.setLevel(Level.WARN);
+            return Level.WARN.toString();
+        } else if(levelString.equals(Level.ERROR.toString())) {
+            logger.setLevel(Level.ERROR);
+            return Level.ERROR.toString();
+        } else if(levelString.equals(Level.FATAL.toString())) {
+            logger.setLevel(Level.FATAL);
+            return Level.FATAL.toString();
+        } else if(levelString.equals(Level.OFF.toString())) {
+            logger.setLevel(Level.OFF);
+            return Level.OFF.toString();
+        } else {
+            return LogSetup.UNKNOWN_LEVEL;
+        }
+    }
+
+    private void printReply(String reply){
+        System.out.println(PROMPT + "Reply: " +  reply);
+    }
+
+    private void printError(String error){
+        System.out.println(PROMPT + "Error: " +  error);
+    }
+
+    /**
+     * Main entry point for the KV Client application.
+     * @param args contains the port number at args[0].
+     */
+    public static void main(String[] args) {
+        try {
+            new LogSetup("logs/client.log", Level.OFF);
+            KVClient client = new KVClient();
+            client.run();
+        } catch (IOException e) {
+            System.out.println("Error! Unable to initialize logger!");
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
