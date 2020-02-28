3bce42701d8b719e31beee6c8b17632e9cd9d946
wheresahmed <a.abdulkadir@mail.utoronto.ca>
2020-02-27 20:12:00 -0500

    add logger and testing

---
+++ m2/src/testing/AllTests.java
@@ -0,0 +1,34 @@
+package testing;
+
+import java.io.IOException;
+
+import app_kvServer.TServer;
+import org.apache.log4j.Level;
+
+import app_kvServer.KVServer;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import logger.LogSetup;
+
+
+public class AllTests {
+
+	static {
+		try {
+			new LogSetup("logs/testing/test.log", Level.ERROR);
+			new TServer(new KVServer(50000, 10, "FIFO")).start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static Test suite() {
+		TestSuite clientSuite = new TestSuite("Basic Storage ServerTest-Suite");
+		clientSuite.addTestSuite(ConnectionTest.class);
+		clientSuite.addTestSuite(InteractionTest.class); 
+		clientSuite.addTestSuite(AdditionalTest.class); 
+		return clientSuite;
+	}
+	
+}
